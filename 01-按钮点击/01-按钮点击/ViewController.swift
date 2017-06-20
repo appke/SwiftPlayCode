59d463c2bfab7e6a46dc800c46864a058bf09218
angmu <anglimu@163.com>
2017-06-20 14:22:03 +0800

    ‘按钮点击demo’

--- 01-按钮点击/01-按钮点击/ViewController.swift
+++ 01-按钮点击/01-按钮点击/ViewController.swift
@@ -12,14 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        let button = UIButton(type: .custom)
+        
+        button.setTitle("点一下……", for: .normal)
+        button.sizeToFit()
+        button.center = view.center
+        button.setTitleColor(.red, for: .normal)
+        button.addTarget(self, action: #selector(buttonClick), for: .touchUpInside)
+        view.addSubview(button)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func buttonClick() {
+        print("点击按钮");
     }
-
-
 }
 
